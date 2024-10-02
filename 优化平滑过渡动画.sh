0959125052d2b2493fd072a36d78a735168ad138
idyll <liushun968858@gmail.com>
2024-10-02 09:42:57 +0800

    Add files via upload
    Optimize the smooth transition animation to make the animation effect smoother

---
+++ 优化平滑过渡动画.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+optimize_animations() {
+    duration=0.1
+    steps=50    
+    x=0
+    y=0
+    moveX=$(echo "scale=2; 200 / $steps" | bc) 
+    moveY=$(echo "scale=2; 100 / $steps" | bc)
+
+    while [ $i -lt $steps ]; do
+        clear
+        x=$(echo "$x + $moveX" | bc)
+        y=$(echo "$y + $moveY" | bc)
+        sleep $duration
+    done
+}
+
+while true; do
+screen_status=$(dumpsys window | grep "mScreenOn" | grep true)
+if [[ "${screen_status}" ]]; then
+echo "$date *📲- 亮屏运行*"
+    optimize_animations
+	echo "$date *📲- 已优化平滑过渡动画使得动画效果更加流畅*"
+else
+echo "$date *📵- 暗屏状态，跳过优化*"
+fi	
+sleep 1
+done
